8894fde8de72b64d5b1ac752997e9ca48ead6758
onebyteone <floresluera28@gmail.com>
2026-03-22 17:00:05 -0500

    Create chatbot controller and implement getModelInfo function

---
+++ src/controllers/chatbot.controller.js
@@ -0,0 +1,10 @@
+import * as openrouterService from '../services/openrouter.service.js';
+
+export async function getModelInfo(req, res) {
+  try {
+    const modelInfo = await openrouterService.getModelInfo();
+    res.json(modelInfo);
+  } catch (error) {
+    res.status(500).json({ status: false, message:'Error getting model endpoints' });
+  }
+}
